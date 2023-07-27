c668378712430db9ec755f26f05ee8185a981bb5
Itoshiko Nurahman <itoshikonurahman@gmail.com>
2023-07-27 10:12:23 +0700

    Done game mechanic

--- script.js
+++ script.js
@@ -12,33 +12,33 @@ const gameBoard = (() => {
 
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
-      box.addEventListener("click", () => {
-        if (box.innerHTML == "") {
-          box.innerHTML = "X";
-        }
-      });
+      box.addEventListener("click", gameControl.handleClick);
     });
   };
 
-  const updateBoard = () => {
-    if (board == "") {
-      board = "X";
+  const updateBoard = (index, symbol) => {
+    if (board[index] == "") {
+      board[index] = symbol;
+      createBoard();
     } else {
-      console.log("Select another box");
+      alert("Please select another box");
     }
   };
 
-  // const resetBoard = () => {
-  //   board = ["", "", "", "", "", "", "", "", ""];
-  // };
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+  };
+
+  const getBoard = () => board;
 
   return {
     createBoard,
     updateBoard,
+    resetBoard,
+    getBoard,
   };
 })();
 
-// Disini akan dibuat tentang player. Nama, symbol, dan skor
 function createPlayer(name, symbol) {
   return {
     name,
@@ -50,7 +50,7 @@ function createPlayer(name, symbol) {
 const gameControl = (() => {
   let player = [];
   let currentPlayer;
-  let gameends;
+  let gameOver;
 
   const start = () => {
     player = [
@@ -58,41 +58,78 @@ const gameControl = (() => {
       createPlayer(document.querySelector("#player-2").value, "O"),
     ];
     gameBoard.createBoard();
+    currentPlayer = player[0];
+
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+      box.addEventListener("click", handleClick);
+    });
   };
 
-  const checkWinner = () => {
-    winnerPatterns = [
-      // Horizontal
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      // Vertical
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      // Diagonal
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    gameBoard.updateBoard(index, currentPlayer.symbol);
 
-    for (const pattern of winnerPatterns) {
-      let [a, b, c] = pattern;
-      if (pattern[a] === pattern[b] && pattern[a] === pattern[c]) {
-        console.log(`The Winner is ${pattern[a]}`);
-      } else if (gameBoard.board.every((board) => board !== "")) {
-        console.log("TIE!");
-      } else {
-        return null;
-      }
+    if (checkForWinner(gameBoard.getBoard(), currentPlayer.symbol)) {
+      gameOver = true;
+      displayWinner(`${currentPlayer.name} is Win!`);
+    } else if (checkForTie(gameBoard.getBoard())) {
+      gameOver = true;
+      displayWinner("It's a Tie!");
     }
+    currentPlayer = currentPlayer == player[0] ? player[1] : player[0];
+  };
+
+  const reset = () => {
+    gameBoard.resetBoard();
+    gameBoard.createBoard();
+    currentPlayer = player[0];
+    gameOver = false;
+    document.querySelector("#message").innerHTML = "";
   };
 
   return {
     start,
+    reset,
+    handleClick,
   };
 })();
 
+function checkForWinner(board) {
+  winnerPatterns = [
+    // Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winnerPatterns.length; i++) {
+    const [a, b, c] = winnerPatterns[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForTie(board) {
+  return board.every((box) => box !== "");
+}
+
+function displayWinner(message) {
+  document.querySelector("#message").innerHTML = message;
+}
+
 const startBtn = document.querySelector("#start-btn");
 const resetBtn = document.querySelector("#reset-btn");
+// const boards = document.querySelectorAll(".box");
 
 startBtn.addEventListener("click", gameControl.start);
+resetBtn.addEventListener("click", gameControl.reset);
